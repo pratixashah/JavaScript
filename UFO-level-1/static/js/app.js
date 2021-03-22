1bfcedc47a473104e0d9cf543cfb00ba2e42d908
pratixashah <shah.pratixa@gmail.com>
2021-03-22 14:08:42 -0700

    Display data on the Web page

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,30 @@
+//from data.js
+var tableData = data;
+//console.log(tableData)
+
+var table_ufo = d3.select("#ufo-table")
+//console.log(table_ufo)
+
+let thead_ufo = table_ufo.select("thead tr").selectAll(".table-head")
+
+console.log(thead_ufo)
+
+
+var tbody = table_ufo.select("tbody")
+
+
+data.forEach((item) => {
+
+  // Append one table row per record
+  var row = tbody.append("tr");
+
+  // append one cell for each column
+  row.append("td").text(item.datetime);
+  row.append("td").text(item.city);
+  row.append("td").text(item.state);
+  row.append("td").text(item.country);
+  row.append("td").text(item.shape);
+  row.append("td").text(item.durationMinutes);
+  row.append("td").text(item.comments);
+});
+
