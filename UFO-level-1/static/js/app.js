ec2afa8f1aec833031de5d7c4a0f0753b45f341f
pratixashah <shah.pratixa@gmail.com>
2021-03-23 14:11:45 -0700

    Displays list of records for selected date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,64 +1,44 @@
 //from data.js
-var tableData = data;
+//var tableData = data;
 //console.log(tableData)
 
 var table_ufo = d3.select("#ufo-table")
 //console.log(table_ufo)
 
 let thead_ufo = table_ufo.select("thead tr").selectAll(".table-head")
-
 // console.log(thead_ufo)
 
+function create_table(data)
+{
+  var tbody = table_ufo.select("tbody")
 
-var tbody = table_ufo.select("tbody")
+  tbody.html("");
 
-data.forEach((item) => {
+  data.forEach((item) => {
 
-  // Append one table row per record
-  var row = tbody.append("tr");
+    // Append one table row per record
+    var row = tbody.append("tr");
 
-  // append one cell for each column
-  row.append("td").text(item.datetime);
-  row.append("td").text(item.city);
-  row.append("td").text(item.state);
-  row.append("td").text(item.country);
-  row.append("td").text(item.shape);
-  row.append("td").text(item.durationMinutes);
-  row.append("td").text(item.comments);
-});
+    // append one cell for each column
+    row.append("td").text(item.datetime);
+    row.append("td").text(item.city);
+    row.append("td").text(item.state);
+    row.append("td").text(item.country);
+    row.append("td").text(item.shape);
+    row.append("td").text(item.durationMinutes);
+    row.append("td").text(item.comments);
+  });
+}
+
+create_table(data)
 
 let button = d3.select("#filter-btn")
 
-button.on("click", function(){
+button.on("click", function()
+  {
     let user_date = d3.select("#datetime")._groups[0][0].value;
 
-    console.log(user_date);
-
-    console.log(tbody._groups[0][0]);
-
     let selected_data = data.filter((row) => row.datetime === user_date);
-    
-    console.log(selected_data);
 
-    
-    // for(let i=0;i<= tbody._groups[0][0].rows.length-1; i++ )
-    // {
-    //   let table_date = tbody._groups[0][0].rows[i].cells[0].innerHTML
-    //   // console.log(tbody._groups[0][0].rows)
-
-    //   selected_date_data= []
-
-    //   // data.forEach((item) => {
-    //   //   if(table_date === item.datetime)
-    //   //   {
-    //   //     selected_date_data.append()
-    //   //   }
-    //   // }
-      
-    //   // if(date === table_date)
-    //   // {
-    //   //   table_date.attr("style", "font-family: Courie");
-    //   //   // style("background-color", "red")
-    //   // }
-    // }
+    create_table(selected_data)
 });
